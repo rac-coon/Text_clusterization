b6428554237351d5eabb70b0485bf33f8605e908
rac-coon <you@example.com>
2022-12-22 23:57:56 +0300

    do code beautifier

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ from matplotlib import pyplot as plt
 from sklearn_som.som import SOM
 
 text_path = "texts/rand/"
-#doc_names = ['1000.docx', '1001.docx', '1002.docx', '1003.docx', '1004.docx', '2000.docx',
-#             '3000.docx', '3001.docx', '3002.docx', '3003.docx', '3004.docx', ]
-doc_names = []
-MAX_TITLE_LEN = 70
+#doc_names = []
+doc_names = ['1000.docx', '1001.docx', '1002.docx', '1004.docx', '2000.docx',
+             '3000.docx', '3001.docx', '3002.docx', '3003.docx', '3004.docx', ]
 
 # Получение путей к файлам
 def get_doc_names():
@@ -29,8 +28,7 @@ def get_doc_names():
     for file in dirs:
         if file.find('.docx') == -1:
             dirs.remove(file)
-    # ограничение количества файлов для проверки результатов
-    doc_names.append(file)
+    #doc_names.append(file)
     return doc_names
 
 def doc_to_string(doc_name):
@@ -59,53 +57,58 @@ def normalization(text_from_doc):
             clear_text += token.lemma_ + ' '
     return clear_text
 
-def result(clear_texts):
-    vectorizer = TfidfVectorizer(ngram_range=(3, 4), min_df=0.01, max_df=0.95, sublinear_tf=True, norm='l2')
+def tfidf(clear_texts):
+    vectorizer = TfidfVectorizer(ngram_range=(3, 4), sublinear_tf=True, norm='l2')
+    # min_df=0.01, max_df=0.95
     vectors = vectorizer.fit_transform(clear_texts)
-    Y = TSNE(n_components=2, random_state=0, perplexity=15, init='pca', learning_rate=200).fit_transform(np.array(vectors.toarray()))
-    vec_x = Y[:, 0]
-    vec_y = Y[:, 1]
-    # plt.scatter(vec_x, vec_y, marker='o')
-    # plt.show()
+    x_embedded = TSNE(n_components=2, random_state=0, perplexity=15, init='pca', learning_rate=200).\
+        fit_transform(np.array(vectors.toarray()))
+    #vec_x = x_embedded[:, 0]
+    #vec_y = x_embedded[:, 1]
+    x_principal = pd.DataFrame(x_embedded)
+    return x_embedded, x_principal
 
-    X_principal = pd.DataFrame(Y)
-    X_principal.columns = ['P1', 'P2']
+def dendrogramm(x_principal):
+    x_principal.columns = ['P1', 'P2']
     plt.figure(figsize=(8, 8))
-
     plt.title('Дендрограмма алгоритма кластеризации Agglomerative Clustering')
-    Dendrogram = shc.dendrogram(shc.linkage(X_principal, method='ward'))
+    Dendrogram = shc.dendrogram(shc.linkage(x_principal, method='ward'))
 
-
+def agglomerative(x_principal):
     ac = AgglomerativeClustering(n_clusters=3, linkage='ward', affinity='euclidean')
-
     plt.figure(figsize=(6, 6))
-    plt.scatter(X_principal['P1'], X_principal['P2'], c=ac.fit_predict(X_principal), cmap='rainbow', marker='o')
+    plt.scatter(x_principal['P1'], x_principal['P2'], c=ac.fit_predict(x_principal), cmap='rainbow', marker='o')
     plt.title('Agglomerative Clustering\nКол-во кластеров: {}'.format(ac.n_clusters))
     plt.show()
 
-
-    X_principal = pd.DataFrame(Y)
-    X_principal.columns = ['P1', 'P2']
+def maps(x_embedded):
+    x_principal = pd.DataFrame(x_embedded)
+    x_principal.columns = ['P1', 'P2']
     Y_som = SOM(m=2, n=2, dim=3)
-    # m - The shape along dimension 0 (vertical) of the SOM
-    # n - The shape along dimesnion 1 (horizontal) of the SOM
-    # dim - The dimensionality (number of features) of the input space
-    # Ir - The initial step size for updating the SOM weights.
 
-    Y_som.fit(Y)
-    predictions = Y_som.predict(Y)
-    plt.scatter(X_principal['P1'], X_principal['P2'], c=predictions, cmap='rainbow', marker='o')
+    '''
+    m - The shape along dimension 0 (vertical) of the SOM
+    n - The shape along dimesnion 1 (horizontal) of the SOM
+    dim - The dimensionality (number of features) of the input space
+    Ir - The initial step size for updating the SOM weights.
+    '''
+
+    Y_som.fit(x_embedded)
+    predictions = Y_som.predict(x_embedded)
+    plt.scatter(x_principal['P1'], x_principal['P2'], c=predictions, cmap='rainbow', marker='o')
     plt.title('Self-organizing maps\nКол-во кластеров:{}'.format(Y_som.m))
     plt.show()
 
-doc_names_list = get_doc_names()
-clear_texts = []
-for doc in doc_names_list:
-    text = doc_to_string(doc)
-    clear_doc_text = normalization(text)
-    clear_texts.append(clear_doc_text)
-    print(doc)
-result(clear_texts)
+
+if __name__ == '__main__':
+    doc_names_list = get_doc_names()
+    clear_texts = []
+    for doc in doc_names_list:
+        text = doc_to_string(doc)
+        clear_doc_text = normalization(text)
+        clear_texts.append(clear_doc_text)
+    x_embedded, x_principal = tfidf(clear_texts)
+
 
 
 
